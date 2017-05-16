d03a62815ef9d25ad0f85049d9b9b7d0386f1562
Darkspine77 <Jwoods@Riverdale.edu>
2017-05-15 22:18:05 -0400

    Added a buffer time for uploading files

--- js/index.js
+++ js/index.js
@@ -16,7 +16,9 @@ $("#displaySubmit").hide();
         $("#displayOutput").empty()
         $("#submitInput").show();
         $("#textInput").val("");
-        document.getElementById("fileInput").files[0] = null;
+        if(document.getElementById("fileInput").files.length > 0){
+            document.getElementById("fileInput").value = null
+        }
         $("#upload").hide();
         $("#retrieve").show();
         $("#displaySubmit").show();
@@ -46,11 +48,12 @@ $("#displaySubmit").hide();
         }
         if($("#textInput").val() == ""){
             key = makeid();
-            uploadFile(key,document.getElementById("fileInput").files[0]);
-        }
-        $("#displayOutput").append(
-                        '<p align="center"> Your retrieval key is: ' + key + '<p>'
-        );   
+            uploadFile(key,document.getElementById("fileInput").files[0],function(){
+                $("#displayOutput").append(
+                    '<p align="center"> Your retrieval key is: ' + key + '<p>'
+                );
+            });
+        }   
     });
     $("#retrieveData").click(function()
     {   
@@ -79,7 +82,7 @@ function uploadText(key,input){
             });
 }
 
-function uploadFile(key,input){
+function uploadFile(key,input,callback){
     console.log(input);
     var uploadTask = firebase.storage().ref('files/' + key).put(input);
         uploadTask.on('state_changed', function(snapshot){
@@ -89,6 +92,7 @@ function uploadFile(key,input){
             firebase.database().ref('balloons/' + key).update({
                 'object':img
             });
+            callback()
         });
 }
 
@@ -97,14 +101,130 @@ function retrieveData(key){
     var balloon = firebase.database().ref('balloons/' + key);   
     balloon.once('value').then(function(snapshot) 
     {
-        if(snapshot.val().object != undefined){
+        console.log(snapshot.val())
+        if(!snapshot.val().object.startsWith("https://")){
         $("#retrieveOutput").append(
                        '<img  src="images/RetrievedText.png" class="center"><p align="center">' + snapshot.val().object + '<p>'
-    	           );
+                   );
         } else {
             $("#retrieveOutput").append(
                       '<img  src="images/RetrievedFile.png" class="center"><p align="center"><a href="' + snapshot.val().object + '">Link to file</a><p>'
-    	           );
+                   );
+        }
+    });
+}
+  var config = {
+    apiKey: "AIzaSyDCKr77iFVQAP_3LBq1yWhhO6bB-y0iWZw",
+    authDomain: "balloon-659f1.firebaseapp.com",
+    databaseURL: "https://balloon-659f1.firebaseio.com",
+    storageBucket: "balloon-659f1.appspot.com",
+    messagingSenderId: "354031064986"
+  };
+  firebase.initializeApp(config);
+
+$("#displayRetrieve").hide();
+$("#displaySubmit").hide();
+
+(function()
+{   
+    $("#upload").click(function(){ 
+        $("#displayOutput").empty()
+        $("#submitInput").show();
+        $("#textInput").val("");
+        if(document.getElementById("fileInput").files.length > 0){
+            document.getElementById("fileInput").value = null
+        }
+        $("#upload").hide();
+        $("#retrieve").show();
+        $("#displaySubmit").show();
+        $("#displayRetrieve").hide();
+    });
+    
+    $("#retrieve").click(function()
+    {   
+        $("#retrieveOutput").empty();
+        $("#retrieveInput").show();
+        $("#keyInput").val("");
+        $("#retrieve").hide();
+        $("#upload").show();
+        $("#displayRetrieve").show();
+        $("#displaySubmit").hide();
+        
+    });
+    
+    $("#submit").click(function()
+    {   
+        $("#submitInput").hide();
+        var key;
+        if(document.getElementById("fileInput").files[0] == null){
+            key = makeid();
+            uploadText(key,$("#textInput").val());
+            
+        }
+        if($("#textInput").val() == ""){
+            key = makeid();
+            uploadFile(key,document.getElementById("fileInput").files[0],function(){
+                $("#displayOutput").append(
+                    '<p align="center"> Your retrieval key is: ' + key + '<p>'
+                );
+            });
+        }   
+    });
+    $("#retrieveData").click(function()
+    {   
+        $("#retrieveInput").hide();
+        var key = $("#keyInput").val();
+        var data = retrieveData(key);
+    });
+})();
+
+function makeid()
+{
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 7; i++ ){
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+}
+
+function uploadText(key,input){
+    var space = firebase.database().ref('balloons/' + key);
+            space.update({
+                    'object': input
+            });
+}
+
+function uploadFile(key,input,callback){
+    console.log(input);
+    var uploadTask = firebase.storage().ref('files/' + key).put(input);
+        uploadTask.on('state_changed', function(snapshot){
+        }, function(error) {
+        }, function() {
+            var img = uploadTask.snapshot.downloadURL;
+            firebase.database().ref('balloons/' + key).update({
+                'object':img
+            });
+            callback()
+        });
+}
+
+function retrieveData(key){
+    var returnVal;
+    var balloon = firebase.database().ref('balloons/' + key);   
+    balloon.once('value').then(function(snapshot) 
+    {
+        console.log(snapshot.val())
+        if(!snapshot.val().object.startsWith("https://")){
+        $("#retrieveOutput").append(
+                       '<img  src="images/RetrievedText.png" class="center"><p align="center">' + snapshot.val().object + '<p>'
+                   );
+        } else {
+            $("#retrieveOutput").append(
+                      '<img  src="images/RetrievedFile.png" class="center"><p align="center"><a href="' + snapshot.val().object + '">Link to file</a><p>'
+                   );
         }
     });
 }
